58cfdddd129e47afff9368dcd58858136a9e620d
Abhishek Mathur <mathur17021play@gmail.com>
2022-01-12 16:22:39 +0530

    1. SSL Support 2. POST request 3. Making GET AND POST request on the same TCP Connection by reusing it

--- FlixedAssignment 1/Program.cs
+++ FlixedAssignment 1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Net.Security;
 using System.Net.Sockets;
 using System.Text;
 
@@ -6,8 +7,8 @@ namespace AbhishekAssignment
 {
     class AbhishekClient
     {
-        private const string Hostname = "www.google.com";
-        private const int PortNumber = 80;
+        private const string Hostname = "en8fm6n2p76as.x.pipedream.net";
+        private const int PortNumber = 443;
 
         public AbhishekClient()
         {
@@ -17,16 +18,23 @@ namespace AbhishekAssignment
         private Dictionary<string, string> getDefaultHeaders(string host)
         {
             Dictionary<string, string> headers = new Dictionary<string, string>();
-            headers.Add("Accept", "text/html");
-            headers.Add("Connection", "close");
             headers.Add("Host", host);
-            headers.Add("User-Agent", "Abhishek-Client");
+            headers.Add("Connection", "keep-alive");
+            headers.Add("User-Agent", "Abhishek Client 2.0");
+            headers.Add("Accept", "*/*");
+            headers.Add("Origin", "https://requestbin.com");
+            headers.Add("Sec-Fetch-Site", "cross-site");
+            headers.Add("Sec-Fetch-Mode", "cors");
+            headers.Add("Sec-Fetch-Dest", "empty");
+            headers.Add("Referer", "https://requestbin.com/");
+            headers.Add("Accept-Encoding", "gzip, deflate, br");
+            headers.Add("Accept-Language","en-US,en;q=0.9");
             return headers;
         }
 
-        private string getRequestPayload(Dictionary<string, string> headers)
+        private string getRequestPayload(Dictionary<string, string> headers,string method,string host,string protocol,string path)
         {
-            string message = "GET / HTTP/1.1\r\n";
+            string message = $"{method} {protocol}://{host}/{path} HTTP/1.1\r\n";
             foreach ( (string headerName, string headerValue) in headers)
             {
                 message += $"{headerName}: {headerValue}\r\n";
@@ -35,30 +43,65 @@ namespace AbhishekAssignment
             return message;
         }
 
+        private void RunGETRequest(string host, StreamReader reader, StreamWriter writer)
+        {
+            Console.WriteLine("=====================================  GET REQUEST ==================================================");
+            string requestPayload = getRequestPayload(getDefaultHeaders(host),"GET",host,protocol:"https",path:"");
+            writer.Write(requestPayload);
+            writer.Flush();
+            PrintResponse(reader);
+            Console.WriteLine("=====================================  GET REQUEST END ==================================================");
+        }
+
+        private void RunPOSTRequest(string host, StreamReader reader, StreamWriter writer)
+        {
+            Console.WriteLine("=====================================  POST REQUEST ==================================================");
+            string sampleJSON = "{\"message\":\"Hello, I am Abhishek Client\"}";
+            Dictionary<string, string> baseHeaders = getDefaultHeaders(host);
+            baseHeaders.Add("content-type","application/json");
+            baseHeaders.Add("Content-Length",sampleJSON.Length.ToString());
+            string requestPayloadBase = getRequestPayload(baseHeaders,"POST",host,protocol:"https",path:"");
+            string fullPostPayload = requestPayloadBase + sampleJSON;
+            writer.Write(fullPostPayload);
+            writer.Flush();
+            PrintResponse(reader);
+            Console.WriteLine("=====================================  POST REQUEST END ==================================================");
+        }
+        
         private void Run(string host, int port)
         {
             try
             {
-                string requestPayload = getRequestPayload(getDefaultHeaders(host));
                 TcpClient client = new TcpClient();
-                client.Connect(host, 80);
-                NetworkStream networkStream = client.GetStream();
-                networkStream.ReadTimeout = 2000;
-                StreamWriter writer = new StreamWriter(networkStream);
-                StreamReader reader = new StreamReader(networkStream, Encoding.UTF8);
-                byte[] bytes = Encoding.UTF8.GetBytes(requestPayload);
-                networkStream.Write(bytes, 0, bytes.Length);
-                string response = reader.ReadToEnd();
-                Console.WriteLine(response);
-                /**
-                 * By using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http, I can extract body
-                 * https://stackoverflow.com/questions/318506/converting-raw-http-request-into-httpwebrequest-object
-                 */
+                client.Connect(host, port);
+                SslStream sslStream = new SslStream(client.GetStream(),true);
+                sslStream.AuthenticateAsClient(host);
+                sslStream.ReadTimeout = 2000;
+                StreamWriter writer = new StreamWriter(sslStream);
+                writer.AutoFlush = true;
+                StreamReader reader = new StreamReader(sslStream);
+                RunPOSTRequest(host,reader,writer);
+                RunGETRequest(host,reader,writer);
+                reader.Close();
+                writer.Close();
+                sslStream.Close();
+                client.Close();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
+        }
+
+        private void PrintResponse(StreamReader reader)
+        {
+            char[] responseBuffer = new char[1024];
+            reader.Read(responseBuffer,0,responseBuffer.Length);
+            for (int characterIndex = 0; characterIndex < responseBuffer.Length; characterIndex++)
+            {
+                Console.Write(responseBuffer[characterIndex]);
+            } 
+            Console.WriteLine("");
         }
 
     }
